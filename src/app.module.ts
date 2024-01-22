e6e32139a55dff78087a37c72c642ae730c0e9d5
Vadym Semcheno <vadym.semchenko@guesty.com>
2024-01-22 23:57:42 +0200

    connect to postgres remote db

---
+++ src/app.module.ts
@@ -0,0 +1,49 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
+// import { WalletModule } from './wallet/wallet.module'
+// import { Wallet } from './wallet/wallet.entity';
+// import { TransactionModule } from './transaction/transaction.module'
+// import { Transaction } from './transaction/transaction.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-34-241-82-91.eu-west-1.compute.amazonaws.com',
+      // host: process.env.HOST,
+      url: 'postgres://mupzkvpitzcdgh:5df4c61bfd44cc487638b5b68f51f906aa0b1996946a468687103ff2abb7444a@ec2-34-241-82-91.eu-west-1.compute.amazonaws.com:5432/delodbvpc5bl7b',
+      // url: process.env.DATABASE_URL,
+      port: 5432,
+      // port: +process.env.PORT,
+      password: '5df4c61bfd44cc487638b5b68f51f906aa0b1996946a468687103ff2abb7444a',
+      // password: process.env.PASSWORD,
+      username: 'mupzkvpitzcdgh',
+      // username: process.env.USERNAME,
+      // entities: [Wallet, Transaction],
+      database: 'delodbvpc5bl7b',
+      // database: process.env.DATABASE,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true
+    }),
+    // WalletModule, TransactionModule
+  ],
+})
+export class AppModule { }
+
+
